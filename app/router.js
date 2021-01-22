1d9ef742c8c3e341aa62448cba10180e75dba961
MathisDelrue51 <mathis.delrue@gmail.com>
2021-01-22 11:04:39 +0100

    dynamisation des routes

--- app/router.js
+++ app/router.js
@@ -1,12 +1,43 @@
-const { request, response } = require('express');
+const {
+    request,
+    response
+} = require('express');
 const express = require('express');
 
 const router = express.Router();
 
 const articles = require('../data/article.json');
 
-router.get('/', (request, response) =>{
-    response.render('index', {articles});
+router.get('/', (request, response) => {
+    response.render('index', {
+        articles
+    });
+});
+
+router.get('/article/:id', (request, response) => {
+    const idArticle = request.params.id;
+
+    let articleExist;
+
+    for (const article of articles) {
+        if (idArticle == article.id) {
+            articleExist = article;
+            
+        }
+    }    
+
+    //SI l'article existe on affiche la vue de l'article
+    if (articleExist !== undefined) {
+        response.render('article', {
+            articles,
+            articleExist
+        });
+    } else {
+        //SINON on change le statut à 404 et on affiche un message
+        response.status(404).send('Error 404: Nous ne trouvons pas l\'article correspondant');
+    }
+
+    
 });
 
 module.exports = router;
