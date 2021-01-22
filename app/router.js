fcac5a65a3b6dcfc5735154b2f93fca6baa161fc
MathisDelrue51 <mathis.delrue@gmail.com>
2021-01-22 09:22:46 +0100

    mise en place de la page d'accueil, lorsque je change les liens vers le dossier static ca retire les styles. Revenir dessus plus tard

--- app/router.js
+++ app/router.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (request, response) =>{
-    response.send('salut');
+    response.render('index');
 });
 
 module.exports = router;
