33e754a11e1521a758883e749367be314f92013e
MathisDelrue51 <mathis.delrue@gmail.com>
2021-01-22 12:02:44 +0100

    ajout d'un fichier middlewares contenant les fonctions de lecture du fichier Json et d'erreur

---
+++ app/middlewares.js
@@ -0,0 +1,18 @@
+function error(request, response, next) {
+    response.status(404).render('404');
+};
+
+function lectureArticleJson(request, response, next) {
+    //lire le fichier article.json
+    const articles = require('../data/article.json');
+    //pour rendre les infos disponibles dans toutes les vues, on va ajouter le contenu de article.json à l'object response.locals
+    //Ainsi, les infos stockées dans cet object seront dispos dans tous les middlewares que vont traverser request et response avant de renvoyer une réponse au navigateur
+    response.locals.articles = articles;
+    next();
+}
+
+
+module.exports = {
+    error,
+   lectureArticleJson
+};
--- index.js
+++ index.js
@@ -6,16 +6,22 @@ const PORT = 4500;
 
 const router = require('./app/router');
 
+const middlewares = require('./app/middlewares');
+
 
 //on indique à express le moteur de temlplates à utiliser
 app.set('view engine', 'ejs');
 //on ajoute le répertoire des vues
 app.set('views', './views');
 
+app.use(middlewares.lectureArticleJson);
+
 //on indique le répertoire pour les ressources statiques
 app.use(express.static('./static'));
 
 app.use(router);
+
+app.use(middlewares.error);
 
 
 app.listen(PORT, () => {
